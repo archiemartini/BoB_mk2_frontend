6286ae1931b8d884dfbe762a0d3e48b8ed47370f
archiemartini <archie.cgm@gmail.com>
2022-09-19 11:52:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import api from './api/base'
 
 function App() {
+
+  const [mealData, setMealData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.post('./meals')
+        setMealData(response.data)
+        console.log("mealData", response.data)
+      } catch (err) {
+        console.log(`Error: ${err.message}`)
+      }
+    }
+    fetchData()
+  }, [])
+
+  const mealList = mealData.map((meal) => {
+    return (
+      <div>
+        <img src={meal.image}></img>
+        <span>{meal.likes} likes</span>
+      </div>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      {mealList}
     </div>
   );
 }

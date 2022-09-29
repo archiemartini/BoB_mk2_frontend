3a95d1cae6132c8282db3cf5afd1dbcbab2630db
archiemartini <archie.cgm@gmail.com>
2022-09-29 11:53:40 +0100

    provided store

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import { Route, Routes } from "react-router-dom"
+import { Provider } from "react-redux"
+import configureStore from './store/store'
 
-
+const store = configureStore()
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
+    <Provider store={store}>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
